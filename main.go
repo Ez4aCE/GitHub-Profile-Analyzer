61c67d91198ef28fb11bde3cd7aae4390c5e5c34
Ez4aCE <himanshu24rai@gmail.com>
2026-05-23 16:13:39 +0530

    Added derived metrics :follower ratio  and Average followers per repo

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ type GitHubUser struct {
 }
 
 func main() {
-	username := "Ez4aCE"
+	var username string
+	fmt.Print("Enter GitHub username: ")
+	fmt.Scanln(&username)
 	url := "https://api.github.com/users/" + username
 
 	resp, err := http.Get(url)
@@ -53,11 +55,24 @@ func main() {
 		return
 	}
 
+	var ratio float64
+	if user.Following > 0 {
+		ratio = float64(user.Followers) / float64(user.Following)
+	} else {
+		ratio = float64(user.Followers)
+	}
+
+	var followerPerRepo float64
+	if user.Followers > 0 {
+		followerPerRepo = float64(user.Followers) / float64(user.PublicRepos)
+	}
+
 	fmt.Println("👤 GitHub Profile Info")
 	fmt.Printf("Username : %s\n", user.Login)
 	fmt.Printf("Name : %s\n", user.Name)
 	fmt.Printf("Bio : %s\n", user.Bio)
 	fmt.Printf("Public repos : %d\n", user.PublicRepos)
 	fmt.Printf("Followers : %d | Following : %d\n", user.Followers, user.Following)
-
+	fmt.Printf("Follower-to-Following Ratio: %.2f\n", ratio)
+	fmt.Printf("Average Followers per Repo: %.4f\n", followerPerRepo)
 }
